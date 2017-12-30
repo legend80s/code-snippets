24a0e6157045ed63f91e6991ded69a3bede8bd6a
legend80s <liuchzong@qq.com>
2017-12-30 15:16:48 +0800

    Execute promise tasks on by one

--- serialize.js
+++ serialize.js
@@ -1,38 +1,80 @@
 /**
- * 串行运行多个 Promise task
+ * Resource fetch function which returns a Promise
  *
- * @private
+ * @example
+ * const fetchUser = (id) => window.fetch(`http://jsonplaceholder.typicode.com/users/${id}`);
  *
- * @param  {[]Function} tasks 一组返回 Promise 的函数
+ * @callback fetchResource
+ * @param {string} id any resource id
  * @return {Promise}
  */
-function serialize(tasks) {
-  const result = [];
 
-  return tasks.reduce((cur, next) => cur.then((values) => {
-    result.push(...values);
-
-    return next();
-  }), Promise.resolve([]))
-    .then((lastValues) => {
-      result.push(...lastValues);
-
-      return Promise.resolve(result);
+/**
+ * Execute async tasks on by one
+ *
+ * @example
+ * // Example 1
+ * serialize([() => Promise.resolve(1), () => Promise.resolve(2)]).then((results) => console.log('results:', results));
+ * // results: [1, 2]
+ *
+ * // Example 2
+ * // 2 more common use cases in actual development
+ * // Example 2.1
+ * const userIds = [1, 2, 3, 4];
+ * const fetchUser = id => window.fetch(`http://jsonplaceholder.typicode.com/users/${id}`).then((response) => {
+ *   console.log(`GET user#${id} finished`);
+ *   return response.json();
+ * });
+ *
+ * serialize(userIds.map(id => () => fetchUser(id))).then((users) => console.log('users:', users));
+ * // GET user#1 finished
+ * // GET user#2 finished
+ * // GET user#3 finished
+ * // GET user#4 finished
+ * // users: [[{ id: 1 },  { id: 2 }], [ { id: 3 },  { id: 4 }]]
+ *
+ * // Equals to
+ * serialize([
+ *   () => window.fetch('http://jsonplaceholder.typicode.com/users/1')
+ *     .then((response) => { console.log('GET user#1 finished'); return response.json(); }),
+ *
+ *   () => window.fetch('http://jsonplaceholder.typicode.com/users/2')
+ *     .then((response) => { console.log('GET user#2 finished'); return response.json(); }),
+ *
+ *   () => window.fetch('http://jsonplaceholder.typicode.com/users/3')
+ *     .then((response) => { console.log('GET user#3 finished'); return response.json(); }),
+ *
+ *   () => window.fetch('http://jsonplaceholder.typicode.com/users/4')
+ *     .then((response) => { console.log('GET user#4 finished'); return response.json(); }),
+ *  ]).then((users) => console.log('users:', users));
+ *
+ * // Example 2.2
+ * // fetching user information at concurrncy 2
+ * serialize([
+ *   () => Promise.all([
+ *     window.fetch('http://jsonplaceholder.typicode.com/users/1').then((response) => response.json()),
+ *     window.fetch('http://jsonplaceholder.typicode.com/users/2').then((response) => response.json()),
+ *   ]),
+ *   () => Promise.all([
+ *     window.fetch('http://jsonplaceholder.typicode.com/users/3').then((response) => response.json()),
+ *     window.fetch('http://jsonplaceholder.typicode.com/users/4').then((response) => response.json()),
+ *   ]),
+ *  ]).then((users) => console.log('users:', users));
+ * // users: [[{ id: 1 },  { id: 2 }], [ { id: 3 },  { id: 4 }]]
+ *
+ * @param  {fetchResource[]} tasks Tasks to be executed
+ * @return {Promise<Array>}
+ */
+function serialize(tasks = []) {
+  /* eslint-disable arrow-body-style */
+  return tasks.reduce((initialPromise, cur) => {
+    return initialPromise.then((initialValues) => {
+      return cur().then((curValue) => {
+        /* eslint-enable arrow-body-style */
+        return [...initialValues, curValue];
+      });
     });
+  }, Promise.resolve([]));
 }
 
-// example
-const tasks = chunk(appIds, concurrency).map((chunkOfIds) =>
-  () => this.q.all(chunkOfIds.map((appId) =>
-    this.App.get({ appName: rnd(appId), handleError: false }).$promise
-      // 先到先展现
-      .then(({ id, quota, quotaReviewingTask, statistics }) => {
-        this.otherInformationList[id] = { quota, quotaReviewingTask, statistics };
-      })
-      // 出错不影响后面的应用继续获取详情
-      .catch(() => {})
-    )
-  )
-);
-
-serialize(tasks);
+module.exports = serialize;
