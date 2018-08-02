0732a3cbf51b556fdfb6b3b48744dcd8e7263fe6
legend80s <liuchzong@qq.com>
2018-08-02 11:21:33 +0800

    Update rateFilter.js

--- rateFilter.js
+++ rateFilter.js
@@ -4,28 +4,31 @@ export default function rateFilter(filter) {
   /**
    * 将数字转换为增长率
    * 增加正负号和百分号
+   *
    * @param  {number} number 目标数字
    * @param  {number} [fractionSize=2] 保留多少位小数
+   *
    * @return {string}
    *
    * @example
-   * rate(0.125)
-   * // => '+0.13%'
+   * rate(0.12567)
+   * // => '+12.57%'
    *
    * rate(0)
    * // => '+0%'
    *
    * rate(-1.15326)
-   * // => '-1.15%'
+   * // => '-115.33%'
    */
   return function rate(number, fractionSize = 2) {
     /**
      * 小数位被截断
      * @type {string}
      */
-    const truncated = numberFilter(number, fractionSize);
+    const truncated = numberFilter(number * 100, fractionSize);
+    const flag = Number(truncated) >= 0 ? '+' : '';
 
-    return Number(truncated) >= 0 ? `+${truncated}%` : `${truncated}%`;
+    return `${flag}${truncated}%`;
   };
 }
 
