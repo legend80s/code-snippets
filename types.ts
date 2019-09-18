e0ce32350f3de9f3e9ca9032d550684f6921b786
legend80s <liuchzong@qq.com>
2019-09-18 17:25:33 +0800

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,36 @@
+interface Person {
+    id: number;
+    name: string;    
+    lastName: string;
+    load: () => Promise<Person>;
+}
+
+// extract only properties of a given type from Person, for example string
+type P = SubType<Person, string>;
+
+const p: P = {
+    name: '',
+    lastName: '',
+};
+
+
+// Solution
+type FilterKey<Base, SubType> = {
+    [K in keyof Base]: Base[K] extends SubType ? K : never;
+};
+
+type Fx = FilterKey<Person, string>;
+
+type AllowedNames<Base, SubType> = FilterKey<Base, SubType>[keyof Base]
+
+type Ax = AllowedNames<Person, string>;
+
+type SubType<Base, S> = Pick<Base, AllowedNames<Base, S>>
+
+// Solution in one expression
+
+type SubTypeFilter<Base, SubType> = Pick<Base, {
+    [K in keyof Base]: Base[K] extends SubType ? K : never
+}[keyof Base]>
+
+type P2 = SubTypeFilter<Person, string>;
