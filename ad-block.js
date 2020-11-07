97ad61aeb401133e2d7e279a021f98982c31452f
legend80s <liuchzong@qq.com>
2020-11-07 20:40:30 +0800

    Update ad-block.js

--- ad-block.js
+++ ad-block.js
@@ -1,5 +1,9 @@
-$$('*').filter(node =>
-  getComputedStyle(node).zIndex > 100000 ||
-  getComputedStyle(node, 'before').background.includes('http')
-)
+$$('*').filter(node => {
+  const isOuterLink = node.href?.includes('http') && !node.href?.includes(window.location.host);
+
+  return (isOuterLink) ||
+    getComputedStyle(node).zIndex > 100000 ||
+    getComputedStyle(node, 'before').background.includes('http')
+  ;
+})
   .forEach(n => { n.remove() })
