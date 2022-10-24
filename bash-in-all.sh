f6afbf088bbe27e671301db8fb060f2d3faef67f
legend80s <liuchzong@qq.com>
2022-10-24 15:39:58 +0800

    Create bash-in-all.sh

---
+++ bash-in-all.sh
@@ -0,0 +1,18 @@
+# download in batch
+
+#!/bin/bash
+
+files=(
+  "https://xxx.xlsx"
+  "xxxx"
+)
+
+length=${#files[@]}
+
+# https://www.cyberciti.biz/faq/bash-for-loop-array/
+for (( j=0; j<${length}; j++ ));
+do
+  curl -o ${j+1}.xlsx ${files[$j]}
+done
+
+echo "Done ✅"
